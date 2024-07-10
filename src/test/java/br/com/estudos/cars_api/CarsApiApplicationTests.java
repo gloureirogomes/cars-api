25fe36e85859e58ff7206bd23a73f83f8fe6436d
gabzz <loureirobiel@gmail.com>
2024-07-10 19:54:43 -0300

    Adicionando arquivos base do projeto

---
+++ src/test/java/br/com/estudos/cars_api/CarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.cars_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
