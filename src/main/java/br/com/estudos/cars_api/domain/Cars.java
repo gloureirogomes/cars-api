8f25e009ad0cc5bb8d66dbac571596706f775fda
gabzz <loureirobiel@gmail.com>
2024-07-10 22:17:33 -0300

    :sparkles: feat: mudando classe para referenciar tabela do banco de dados
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/domain/Cars.java
+++ src/main/java/br/com/estudos/cars_api/domain/Cars.java
@@ -1,14 +1,43 @@
 package br.com.estudos.cars_api.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "cars")
 public class Cars {
+
+    @Id
+    @Column(name = "car_id")
+    private UUID carId;
+
+    @Column(name = "model")
     private String model;
+
+    @Column(name = "brand")
     private String brand;
+
+    @Column(name = "color")
     private String color;
+
+    @Column(name = "year")
     private Integer year;
+
+    @Column(name = "automatic")
     private Boolean automatic;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
