27e10af72f84e472422c91fbf1fa31f4fdd8de02
gabzz <loureirobiel@gmail.com>
2024-07-10 21:57:34 -0300

    :sparkles: feat: adicionando rota GET para listagem de carros
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/controller/CarsController.java
+++ src/main/java/br/com/estudos/cars_api/controller/CarsController.java
@@ -1,8 +1,14 @@
 package br.com.estudos.cars_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarsController {
+
+    @GetMapping("/cars")
+    public void getCars() {
+        System.out.println("TESTE");
+    }
 
 }
