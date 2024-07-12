45efcbf054dc08af2f234134f16fc2eb0541b88f
gabzz <loureirobiel@gmail.com>
2024-07-11 21:33:45 -0300

    :sparkles: feat: criando dto para body da requisição da criação de carro
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

---
+++ src/main/java/br/com/estudos/cars_api/controller/dto/CarDto.java
@@ -0,0 +1,11 @@
+package br.com.estudos.cars_api.controller.dto;
+
+public class CarDto {
+
+    private String model;
+    private String brand;
+    private String color;
+    private Integer year;
+    private Boolean automatic;
+
+}
