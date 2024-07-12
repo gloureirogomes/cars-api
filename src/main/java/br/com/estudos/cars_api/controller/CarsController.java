4708a1bde25146f50a0f0f4a1c4205578a8ba61e
gabzz <loureirobiel@gmail.com>
2024-07-11 22:57:52 -0300

    :sparkles: feat: implementando rota de criação
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/controller/CarsController.java
+++ src/main/java/br/com/estudos/cars_api/controller/CarsController.java
@@ -1,19 +1,30 @@
 package br.com.estudos.cars_api.controller;
 
-import br.com.estudos.cars_api.domain.Cars;
+import br.com.estudos.cars_api.controller.dto.CarDto;
+import br.com.estudos.cars_api.domain.Car;
+import br.com.estudos.cars_api.mapper.CarMapper;
 import br.com.estudos.cars_api.repository.CarsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CarsController {
 
+    @Autowired
     private CarsRepository carsRepository;
 
     @PostMapping("/car")
-    public void createCar() {
-        Cars car = new Cars();
-        carsRepository.save(car);
+    public ResponseEntity<CarDto> createCar(@RequestBody CarDto carDto) {
+        Car car = CarMapper.toDomain(carDto);
+        Car persistedCar = carsRepository.save(car);
+        CarDto carToReturn = CarMapper.toDto(persistedCar);
+        return ResponseEntity.status(HttpStatus.CREATED).body(carToReturn);
     }
 
 }
