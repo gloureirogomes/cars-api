25df0b0489175e4e2fbcbdf37ec970ea94a7cdcb
gabzz <loureirobiel@gmail.com>
2024-07-11 22:56:51 -0300

    :sparkles: feat: adicionando anotação JsonProperty na classe dto
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/controller/dto/CarDto.java
+++ src/main/java/br/com/estudos/cars_api/controller/dto/CarDto.java
@@ -1,11 +1,26 @@
 package br.com.estudos.cars_api.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class CarDto {
 
+    @JsonProperty("model")
     private String model;
+
+    @JsonProperty("brand")
     private String brand;
+
+    @JsonProperty("color")
     private String color;
+
+    @JsonProperty("year")
     private Integer year;
+
+    @JsonProperty("automatic")
     private Boolean automatic;
 
 }
