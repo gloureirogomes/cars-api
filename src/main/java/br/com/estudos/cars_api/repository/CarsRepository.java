e6652ab5ef4610effb25ac6b23901f3e07229af6
gabzz <loureirobiel@gmail.com>
2024-07-19 19:46:01 -0300

    :sparkles: refact: refatorando rota de consulta por id
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/repository/CarsRepository.java
+++ src/main/java/br/com/estudos/cars_api/repository/CarsRepository.java
@@ -4,12 +4,13 @@ import br.com.estudos.cars_api.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CarsRepository extends JpaRepository<Car, UUID> {
     Car save(Car car);
     void deleteById(UUID carId);
-    boolean existsById(UUID carId);
+    Optional<Car> findById(UUID carId);
 }
 
