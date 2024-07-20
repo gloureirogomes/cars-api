9fbdae143f24e0a082e231720e81a210e2e6e273
gabzz <loureirobiel@gmail.com>
2024-07-19 21:45:27 -0300

    :sparkles: refact: refatoração para utilizar classe de serviço
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @GustavoRizerioDev <gustavo.rsouza@sptech.school>

--- src/main/java/br/com/estudos/cars_api/controller/CarsController.java
+++ src/main/java/br/com/estudos/cars_api/controller/CarsController.java
@@ -1,14 +1,19 @@
 package br.com.estudos.cars_api.controller;
 
 import br.com.estudos.cars_api.controller.dto.CarDto;
-import br.com.estudos.cars_api.domain.Car;
-import br.com.estudos.cars_api.mapper.CarMapper;
-import br.com.estudos.cars_api.repository.CarsRepository;
+import br.com.estudos.cars_api.exception.CarNotFoundException;
+import br.com.estudos.cars_api.exception.SaveCarException;
+import br.com.estudos.cars_api.service.CarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -16,29 +21,33 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CarsController {
 
-    @Autowired
-    private CarsRepository carsRepository;
+    private final CarService carService;
 
     @PostMapping("/car")
     public ResponseEntity<CarDto> createCar(@RequestBody CarDto carDto) {
-        Car car = CarMapper.toDomain(carDto);
-        Car persistedCar = carsRepository.save(car);
-        CarDto carToReturn = CarMapper.toDto(persistedCar);
-        return ResponseEntity.status(HttpStatus.CREATED).body(carToReturn);
+        try {
+            var carToReturn = carService.saveCar(carDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(carToReturn);
+        } catch (SaveCarException sce) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao inserir no banco de dados");
+        }
     }
 
     @DeleteMapping("/car/{carId}")
     public ResponseEntity<Void> deleteCar(@PathVariable UUID carId) {
-        carsRepository.deleteById(carId);
+        carService.deleteCar(carId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/car/{carId}")
-    public ResponseEntity<Car> findCar(@PathVariable UUID carId) {
-        carsRepository.existsById(carId);
-        return ResponseEntity.ok((carsRepository.findById(carId)).orElse(null));
+    public ResponseEntity<CarDto> findCar(@PathVariable UUID carId) {
+        try {
+            var carToReturn = carService.findCarById(carId);
+            return ResponseEntity.ok((carToReturn));
+        } catch (CarNotFoundException cnfe) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Registro não existe no banco");
+        }
     }
-
 }
 
 
